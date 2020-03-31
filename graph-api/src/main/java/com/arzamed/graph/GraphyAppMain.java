15e7fa3841464f19e723ce5022ed144daf80be33
Rana Das <rana.pratap.das@gmail.com>
2020-03-31 22:56:41 +0100

    Spring boot app main

---
+++ graph-api/src/main/java/com/arzamed/graph/GraphyAppMain.java
@@ -0,0 +1,11 @@
+package com.arzamed.graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphyAppMain {
+    public static void main(String[] args) {
+        SpringApplication.run(GraphyAppMain.class, args);
+    }
+}
---
+++ graph-api/src/main/java/com/arzamed/graph/config/GraphyConfig.java
@@ -0,0 +1,20 @@
+package com.arzamed.graph.config;
+
+import com.arangodb.ArangoDB;
+import com.arangodb.springframework.annotation.EnableArangoRepositories;
+import com.arangodb.springframework.config.ArangoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableArangoRepositories(basePackages = {"com.arzamed.graph.model"})
+public class GraphyConfig implements ArangoConfiguration {
+    @Override
+    public ArangoDB.Builder arango() {
+        return new ArangoDB.Builder().host("localhost", 8529).password("openSesame").user("root");
+    }
+
+    @Override
+    public String database() {
+        return "graphy-arza";
+    }
+}
